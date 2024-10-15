500b5a6074e810ccf5e5c3dba52098c921aaa88f
Vikas-tech-hash <bablu.patil35@gmail.com>
2024-10-15 14:11:06 +0530

    Create Car.java
    commit message

---
+++ Car.java
@@ -0,0 +1,31 @@
+public class Car
+{
+    String color;
+    String model;
+    int cost;
+    int milage;
+    
+    public 
+    
+    
+    public void startCar()
+    {
+        System.out.println("Car is Start"+model);
+        
+    }
+   public void stopCar()
+   {
+       System.out.println("Car is Stop"+model);
+       
+   }
+   public void carDetails()
+   {
+       System.out.println("Model of the car is"+model);
+       System.out.println("color of the car is"+color);
+       System.out.println("cost of the car is"+cost);
+       System.out.println("milage of the car is"+milage);
+       
+   }
+
+  }
+    
